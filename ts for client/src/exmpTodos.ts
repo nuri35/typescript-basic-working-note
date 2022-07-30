384da7004e9b3bc08e6b9b7470f18f98f826fc2c
Nurettin Şen <nurie487@gmail.com>
2022-07-30 12:08:17 +0300

    Set

--- ts for client/src/exmpTodos.ts
+++ ts for client/src/exmpTodos.ts
@@ -1,9 +1,3 @@
-interface TodoExm {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-}
 //sımdı ben burda fetch'e tıp destegı veremıyorum gidip böyle demem lazım   const json : TodoExmp = await response.json(); ama bunu nasıl otomatıklestırebılrız fetch üzerine geldıgımızde tipler tanımlamış ordan yola çıkarak denemeFnType fonksıyonu gibi yapacaz cunku fetch'e bız tür tanımlaması veremıyoruz onun kendınsın tür tanımlamasını böyle kulanabılrız
 export const denemeFn = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
@@ -11,8 +5,11 @@ export const denemeFn = async () => {
   console.log(json);
 };
 
-export const denemeFnType = async (input: RequestInfo, init?: RequestInit) => {
-  const response = await fetch(input, init);
-  const json = await response.json();
-  console.log(json);
-};
+export async function denemeFnType<T extends object | any[]>(
+  input: RequestInfo,
+  init?: RequestInit
+) {
+  const response = await fetch(input, init); //fetch'ın tanımladıgı tıpı verdık
+  const json: T = await response.json(); //burdada json ne turde olcagını verdık ve donus tıpını belırlemıs olduk aslında
+  return json;
+}
--- ts for client/src/main.ts
+++ ts for client/src/main.ts
@@ -35,7 +35,7 @@ function Returner2x<T = number>(par: T) {
   console.log(typeof par);
 }
 
-Returner2x("false"); //hata vermez üstüne gelip bakarsın
+Returner2x(["ada",1]); //hata vermez üstüne gelip bakarsın
 
 function Returner2xx<T = number>(par: string) {
   //T'yi içerde kullanırsın
@@ -142,5 +142,115 @@ let arr = new Array<Producta> // Array'e tıkladıgında gorcenkı <T > seklınd
  import { denemeFn, denemeFnType } from './exmpTodos';
  denemeFn()
 
+ interface TodoExm {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
- denemeFnType("https://jsonplaceholder.typicode.com/todos/4")
+const val =  denemeFnType<TodoExm>("https://jsonplaceholder.typicode.com/todos/4");
+val.then(e=>{
+  console.log(e)
+}).catch(err=>{
+  console.log(err)
+})
+
+
+/***
+ * 
+ * exmp aklıma gelen
+ */ 
+ type ABC<T> = [
+  description: T
+ ];
+
+let myArr : ABC<string|number>
+
+myArr = ["tanımalmadır"]
+
+
+/**
+ * 
+ * bir diziyi tuple yapmak ıstıyorsanız
+ */
+
+
+ type ABCdenem<T> = [
+  description: T
+ ];
+
+let myArr2 : ABCdenem<string|number> // bunu nasıl tuple yaparız
+let forTuple : [string,number]//böyle
+
+
+//this is tuple
+var employee: [number, string] = [1, "Steve"];
+const employee2: [number, string] = [2, "Steve"]; //constdan dolayı 1 kere atama yaparsın onun yerıne let kullanabılrısn
+
+employee2.push() // buna ızın verır ama
+
+//pekı *** bir degerın kendısı olmasını ıstıyorsan bır objede tip degılde şöyle
+
+const myObj = {
+  name:"kadir",
+  age:'22'
+} // tip olarak belırledı
+
+const myObj2x : {
+  name:"kadir",
+  age:'22'
+} = {
+  name:"kadir",
+  age:'22'
+}
+
+
+myObj2x.name = "kadir" //illa kadir olması gerektı buda böyle bilgi olsun 
+
+
+/** 
+ * 
+ * arrayı sımdı readonly yapmak ıstedıgımızde
+*/
+
+const myarxas =  Object.freeze(["ada",2]) // readonly T[] seklınde donduruyormus dolayısıyla  freeze uzerıne gelırsen ne parametresı verıyorsan o turde oluyor <> buranın içi  ama reeadonly seklındeymıs dolaysyla push vs bıle kulanamazsın
+
+// myarxas.push() not use
+
+const myX : readonly any[] = ["as"]
+
+
+
+// myX = 4 hata verır readonly cunku
+
+const myarxasTypeByObj =  Object.freeze({name:"kerım"}) //başka bir freeze'ye denk gelır freeze tıkladıgımzıda  T extends {[idx: string]: U | null | undefined | object} demıs farklı tür verirsen Object.freeze() kısmına baska freeze'ye denk gelır extends dedıgı içinde zaten Object.freeze({name:"kerım"}) ustune geldıgınde extends'dekı kabul olur  tabı buda readonlydır
+
+
+
+//örnek fonsıyonda kullanalım readonylımızı
+
+
+function isExisting<T extends  readonly any[]>(array:T,child:any): ABCdenem<boolean> { //ABCdenem<any> yerıne tuple yapabırlsın [boolen]  tuple kullanmak daha mantıklı olabılır 
+  
+  const index = array.findIndex((value)=>value === child)
+
+  if (index >= 0) {
+    return [true] // true aslında  ABCdenem arrayındekı descriptiona denktır
+    
+  }
+
+  return [false]
+}
+
+
+const [existing] = isExisting(myarxas,25)
+
+console.log(existing) //true yada false verır
+
+/**
+ * 
+ * 
+ * 
+ */
+
